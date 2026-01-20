7de37131ef68d72a149176b81bdd31607e0d42dc
saad <um7554057@gmail.com>
2026-01-20 19:27:40 +0500

    netlify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/kashmala-zeb-portfolio/',
   server: {
     host: "::",
     port: 8080,
